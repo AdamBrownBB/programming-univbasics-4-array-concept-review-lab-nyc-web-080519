87c27cafc9c1b6372fddf478fdb6c8a8a2f479a6
Adam Brown <adam.brown.betty@gmail.com>
2019-08-01 16:28:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+   if array.include?(value_to_find) do
+     return [index]
+   end 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min 
 end
